346a1cdb1a971c0b4ed42b89cf5c1accaacfd068
sthsurendra <38010470+sthsurendra@users.noreply.github.com>
2020-03-17 21:55:36 -0500

    final project of bonusofEmployee

---
+++ Oops_BonusPrograme/src/Employees.java
@@ -0,0 +1,14 @@
+
+public class Employees {
+	int eid;
+	String name;
+	int salary;
+	int bonus;
+	
+	public void bonusforEmployees() {
+		System.out.println("Employee"+" "+name+" "+"gets"+" "+"bonus"+":"+salary*20/100);
+	}
+	
+	
+
+}
---
+++ Oops_BonusPrograme/src/Test_Employee.java
@@ -0,0 +1,20 @@
+
+public class Test_Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employees e = new Employees();
+		Employees f = new Employees();
+		e.eid=201;
+		e.name="roshan";
+		e.salary=100;
+		e.bonusforEmployees();
+		
+		f.eid=301;
+		f.name="jyant";
+		f.salary=200;
+		f.bonusforEmployees();
+
+	}
+
+}
