4efb02a3e7eb3f6a82011209a64c1500bc7f403c
sthsurendra <38010470+sthsurendra@users.noreply.github.com>
2020-03-17 22:16:20 -0500

    update 1

--- Oops_BonusPrograme/src/Employees.java
+++ Oops_BonusPrograme/src/Employees.java
@@ -1,9 +1,12 @@
 
 public class Employees {
+	//properties or variable or employees has eid,name
+	//properties or variable or employees has eid,name
 	int eid;
 	String name;
 	int salary;
 	int bonus;
+	int phone;
 	
 	public void bonusforEmployees() {
 		System.out.println("Employee"+" "+name+" "+"gets"+" "+"bonus"+":"+salary*20/100);
